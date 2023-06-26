40d2bf6d3d3daaa883f7ffb8055ff35d59304615
vaibhav Talkhande <49303222+VaibhavTalkhande@users.noreply.github.com>
2023-06-26 20:17:24 +0530

    02variables

---
+++ 02variables/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+const LoginToken string = "asdasdasdasdasdasdasdasdasd"
+
+func main() {
+	var username string = "vaibhav"
+	fmt.Println("Hello, ", username)
+	fmt.Printf("Variable is of type: %T \n", username)
+	var isLoggedin bool = true
+	fmt.Println("Logged in status: ", isLoggedin)
+	var smallVal uint8 = 255
+	fmt.Println("Max value of uint8: ", smallVal)
+	var smallFloat float32 = 255.455
+	fmt.Println("Small float: ", smallFloat)
+
+
+	// Default values and some aliases
+	var anotherVariable int
+	fmt.Println("Default value of int: ", anotherVariable)
+	//implicit type
+	var website = "learncodeonline.in"
+	fmt.Println("Website: ", website)
+	// no var style
+	numberOfUsers := 300000.00
+	fmt.Println("Number of users: ", numberOfUsers)
+}
